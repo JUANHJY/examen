7342eeaa78bfea7479a2f8268bd290ca378e6c0f
JUANHJY <juanhernandezjose6@gmail.com>
2024-10-04 21:45:28 -0400

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/* Datos de consumo de combustible, las variables que se van a calcular son las */
+/* presio por kilometro, la convercion de litros a galon. */
+/* el procio del combustible=290....... un galon es = 3.78 litros.
+/* un libro = a o.26 galones */
+
+int main()
+
+{  // entrada de tados
+     float  consumo minimo de combustible, maximo, precio g;
+     const float LPG = 3.78; // G3.78 litros
+
+
+     printf("Ingfrese el consumo minimo en L/100 km: ");
+     scanf( "%f" ,  & consumo minimo );
+     printf("Ingresar el consumo maximoen L/100 km: ");
+     scanf("%f" , & consumo maximio )
+     printf("Igrese el consto del galon de combuatible: km")
+     scanf("%f" &presio g);
+
+     // Imprimr tados
+     printf("%"-20s %-20\n"," consumo minimo(L/100 km)", "precio por km");
+     for (float consumo = minimo; consumo <=consumo maximo; consumo++)
+     }
+
+     float precio por km =(consumo / 100) *(precio por g / LPG);
+     printf("%-20.2f %-20.4f\n", consumo, precio por km);
+     }
+
+     return 0;
+     {
+
+     }
